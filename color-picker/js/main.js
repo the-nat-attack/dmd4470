4bfdc4a1625e7dd9e04615b9deafe474bfaead17
Natalie <natalie.lacroix@uconn.edu>
2022-02-02 10:43:39 -0500

    dark mode save

--- color-picker/js/main.js
+++ color-picker/js/main.js
@@ -2,7 +2,14 @@ var opt= document.getElementById('head');
 var btn= document.getElementById('btn');
 var body = document.getElementById('body')
 var dark = document.getElementById('dark')
-var i = 0
+
+if(localStorage.getItem('dark_mode')){
+    var i = 1
+    body.classList.toggle('dark-side')
+}
+else{
+    var i = 0
+}
 
 if(localStorage.getItem('saved_color')){
     opt.value = localStorage.getItem('saved_color')
@@ -21,12 +28,14 @@ function darkSide(){
     if (i===0){
         opt.value = ('#000000')
         i=1
+        localStorage.setItem('dark_mode', 1)
     }
     else{
         opt.value = ('#FFFFFF')
         i=0
+        localStorage.removeItem('dark_mode')
     }
-    localStorage.clear()
+    localStorage.removeItem('saved_color')
     changeColor()
 }
 
