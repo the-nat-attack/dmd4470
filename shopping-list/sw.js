1c2796ff080b7f9d28afdce899599275b3307a72
Natalie <natalie.lacroix@uconn.edu>
2022-02-28 10:38:44 -0500

    trying sw again

--- shopping-list/sw.js
+++ shopping-list/sw.js
@@ -8,7 +8,7 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
-        '/',
+        
         /*
           DEAR READER,
           ADD A LIST OF YOUR ASSETS THAT
